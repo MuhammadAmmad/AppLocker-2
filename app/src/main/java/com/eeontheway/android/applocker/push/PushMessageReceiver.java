3897326f9b7b8c0e191cb62d4020da8c9172f832
guoguo <527676163@qq.com>
2016-02-25 13:11:27 +0800

    完善推送功能

--- app/src/main/java/com/eeontheway/android/applocker/push/PushMessageReceiver.java
+++ app/src/main/java/com/eeontheway/android/applocker/push/PushMessageReceiver.java
@@ -1,15 +1,14 @@
-package com.eeontheway.android.applocker.sdk.getui;
+package com.eeontheway.android.applocker.push;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.igexin.sdk.PushConsts;
 
 /**
- * 推送服务接受器
+ * 个推的推送服务接受器
  * @author lishutong
  * @version v1.0
  * @Time 2016-12-15
@@ -21,12 +20,18 @@ public class PushMessageReceiver extends BroadcastReceiver {
 
         switch (bundle.getInt(PushConsts.CMD_ACTION)) {
             case PushConsts.GET_MSG_DATA:
-                // 获取透传（payload）数据
                 byte[] payload = bundle.getByteArray("payload");
                 if (payload != null) {
-                    String data = new String(payload);
+                    // 调用消息处理器处理消息
+                    PushMessageProcessor processor = new PushMessageProcessor(context);
+                    processor.processMessage(payload);
                 }
                 break;
+            case PushConsts.GET_CLIENTID:
+                String cid = bundle.getString("clientid");
+                GetTuiPush.setClientId(cid);
+                break;
+
             default:
                 break;
         }
